e0b3721e7ce689d968b0a752b5ebb43111d451bd
Matheus Castello <matheus@castello.eng.br>
2026-08-07 00:27:58 -0300

    distro: Add support for Raspberry Pi CM5
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-kernel/linux/dtb.ts
+++ cookbook/recipes-kernel/linux/dtb.ts
@@ -50,6 +50,40 @@ if (MACHINE === "rpi5b") {
             env: process.env
         })
     logger.success("ok, deploy device tree overlaysfor kernel ok")
+
+} else if (MACHINE === "cm5") {
+    execSync(
+        `sudo -k ` +
+        `cp -f ${BUILD_PATH}/tmp/${MACHINE}/linux/arch/arm64/boot/dts/broadcom/bcm2712-rpi-cm5-cm4io.dtb ${IMAGE_MNT_BOOT}/`,
+        {
+            shell: "/bin/bash",
+            stdio: "inherit",
+            encoding: "utf-8",
+            env: process.env
+        })
+    logger.success("ok, deploy device tree for kernel ok")
+
+    execSync(
+        `sudo -k ` +
+        `mkdir -p ${IMAGE_MNT_BOOT}/overlays`,
+        {
+            shell: "/bin/bash",
+            stdio: "inherit",
+            encoding: "utf-8",
+            env: process.env
+        })
+
+    execSync(
+        `sudo -k ` +
+        `cp ${BUILD_PATH}/tmp/${MACHINE}/linux/arch/arm64/boot/dts/overlays/*.dtbo ${IMAGE_MNT_BOOT}/overlays/`,
+        {
+            shell: "/bin/bash",
+            stdio: "inherit",
+            encoding: "utf-8",
+            env: process.env
+        })
+    logger.success("ok, deploy device tree overlaysfor kernel ok")
+
 } else if (MACHINE === "rpi4b") {
     execSync(
         `sudo -k ` +
@@ -82,6 +116,7 @@ if (MACHINE === "rpi5b") {
             env: process.env
         })
     logger.success("ok, deploy device tree overlaysfor kernel ok")
+
 } else {
     logger.warn(`no device tree for linux kernel for ${MACHINE}`)
 }
