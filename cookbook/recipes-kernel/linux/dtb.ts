2312509f8a8f3afc21f99056dc32d6e6b25c610f
Matheus Castello <matheus@castello.eng.br>
2026-08-07 00:03:53 -0300

    linux: dtb: Fix linter issues
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-kernel/linux/dtb.ts
+++ cookbook/recipes-kernel/linux/dtb.ts
@@ -1,24 +1,14 @@
 #!/usr/bin/env -S deno run --allow-all
 
 import PATH from "node:path"
-import FS from "node:fs"
 import logger from "node-color-log"
 import { execSync } from "node:child_process"
 
 // run update in the chroot
 logger.info("deploy Linux kernel device tree ...")
 
-const ARCH = process.env.ARCH as string
 const MACHINE = process.env.MACHINE as string
-const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
 const BUILD_PATH = process.env.BUILD_PATH as string
-const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
-const DISTRO_MINOR = process.env.DISTRO_MINOR as string
-const DISTRO_PATCH = process.env.DISTRO_PATCH as string
-const USER_PASSWD = process.env.USER_PASSWD as string
-
-// read the meta data
-const meta = JSON.parse(process.env.META as string)
 
 // get the actual script path, not the process.cwd
 const _path = PATH.dirname(process.argv[1])
