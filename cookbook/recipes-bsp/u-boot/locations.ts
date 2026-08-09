b2de38c2cb687a71e594353ddd1eee3c3953912b
Matheus Castello <matheus@castello.eng.br>
2026-08-08 22:46:41 -0300

    u-boot: cm5: Fix leftover env and locations not being set correctly
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cookbook/recipes-bsp/u-boot/locations.ts
+++ cookbook/recipes-bsp/u-boot/locations.ts
@@ -1,24 +1,17 @@
 #!/usr/bin/env -S deno run --allow-all
 
 import PATH from "node:path"
-import FS from "node:fs"
 import logger from "node-color-log"
 import { execSync } from "node:child_process"
 
 // run update in the chroot
 logger.info("patch u-boot common/splash.c ...")
 
-const ARCH = process.env.ARCH as string
 const MACHINE = process.env.MACHINE as string
-const MAX_IMG_SIZE = process.env.MAX_IMG_SIZE as string
 const BUILD_PATH = process.env.BUILD_PATH as string
-const DISTRO_MAJOR = process.env.DISTRO_MAJOR as string
-const DISTRO_MINOR = process.env.DISTRO_MINOR as string
-const DISTRO_PATCH = process.env.DISTRO_PATCH as string
-const USER_PASSWD = process.env.USER_PASSWD as string
 
-// read the meta data
-const meta = JSON.parse(process.env.META as string)
+// // read the meta data
+// const meta = JSON.parse(process.env.META as string)
 
 // get the actual script path, not the process.cwd
 const _path = PATH.dirname(process.argv[1])
@@ -28,7 +21,7 @@ const IMAGE_MNT_ROOT = `${BUILD_PATH}/tmp/${MACHINE}/mnt/root`
 process.env.IMAGE_MNT_BOOT = IMAGE_MNT_BOOT
 process.env.IMAGE_MNT_ROOT = IMAGE_MNT_ROOT
 
-if (MACHINE === "rpi4b" || MACHINE === "rpi5b") {
+if (MACHINE === "rpi4b" || MACHINE === "rpi5b" || MACHINE === "cm5") {
     // apply the splash.c
     execSync(
     `sudo -k ` +
